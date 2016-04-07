8249e49c8256d4179fcd67144763e03f7496ad25
acastemoreno <acastemoreno@gmail.com>
2016-04-07 01:00:05 -0500

    finish primera propuesta

--- primera propuesta/js/init.js
+++ primera propuesta/js/init.js
@@ -4,30 +4,55 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $("#foo1").carouFredSel({
-                width: "100%",
-                height: 323,
-                circular: false,
-                infinite: false,
-                pagination  : "#foo1_pag",
-                auto    : false,
-                items: {
-                    width: 360,
-                    height: 320,
-                },
-                scroll: {
-                    items: 1,
-                    duration: 1200,
-                    pauseOnHover: true
-                },
-                prev    : {
-			        button  : "#foo1_prev",
-			        key     : "left"
-			    },
-			    next    : {
-			        button  : "#foo1_next",
-			        key     : "right"
-			    }
-            });
+        width: "100%",
+        height: 323,
+        circular: false,
+        infinite: false,
+        pagination  : "#foo1_pag",
+        auto    : false,
+        items: {
+            width: 360,
+            height: 320,
+        },
+        scroll: {
+            items: 1,
+            duration: 1200,
+            pauseOnHover: true
+        },
+        prev    : {
+	        button  : "#foo1_prev",
+	        key     : "left"
+	    },
+	    next    : {
+	        button  : "#foo1_next",
+	        key     : "right"
+	    }
+    });
+    $("#foo2").carouFredSel({
+        width: "100%",
+        height: 303,
+        circular: false,
+        infinite: false,
+        pagination  : "#foo2_pag",
+        auto    : false,
+        items: {
+            width: 360,
+            height: 330
+        },
+        scroll: {
+            items: 1,
+            duration: 1200,
+            pauseOnHover: true
+        },
+        prev    : {
+            button  : "#foo2_prev",
+            key     : "left"
+        },
+        next    : {
+            button  : "#foo2_next",
+            key     : "right"
+        }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
