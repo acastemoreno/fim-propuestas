6b50fd89a410bb79e2d260b7e5b22c672108647d
acastemoreno <acastemoreno@gmail.com>
2016-04-07 00:02:01 -0500

    slider eventos

--- primera propuesta/js/init.js
+++ primera propuesta/js/init.js
@@ -3,6 +3,31 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $("#foo1").carouFredSel({
+                width: "100%",
+                height: 323,
+                circular: false,
+                infinite: false,
+                pagination  : "#foo1_pag",
+                auto    : false,
+                items: {
+                    width: 360,
+                    height: 320,
+                },
+                scroll: {
+                    items: 1,
+                    duration: 1200,
+                    pauseOnHover: true
+                },
+                prev    : {
+			        button  : "#foo1_prev",
+			        key     : "left"
+			    },
+			    next    : {
+			        button  : "#foo1_next",
+			        key     : "right"
+			    }
+            });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
